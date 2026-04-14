6e5dac85464b5c12ab3d5ff8add65a36f746dbe9
KurugoduSushma <sushmakurugodu2004@gmail.com>
2026-04-14 22:00:22 +0530

    Remove tracked SQL dump from repo and ignore it

--- FRONTEND/db.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE eeg_users;
-USE eeg_users;
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
-);
-
